c043ea730d9ba988582aa56a56f54de228fac613
PinksoulJava31 <safiasaidali31@gmail.com>
2023-07-03 08:12:10 -0700

    first commit

---
+++ src/test/java/com/library/library/management/project/LibraryManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.library.management.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
